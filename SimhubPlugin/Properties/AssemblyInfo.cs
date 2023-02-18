4956a01857c1272e1d480d457183fb5ed60b64f9
Bruce McLeod <bruce@teknologika.com>
2023-02-18 22:53:36 +1100

    Updated assembly version info

--- SimhubPlugin/Properties/AssemblyInfo.cs
+++ SimhubPlugin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("APR.DashSupport")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Average Performance Racing DashSupport")]
+[assembly: AssemblyDescription("Support for APR Simhub Dashes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("APR.DashSupport")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
